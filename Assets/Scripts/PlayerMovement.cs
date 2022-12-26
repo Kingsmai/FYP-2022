dc27b53ba9710cebaf5d442d1f00dd771212f277
Xiaomai <xsbugh@gmail.com>
2022-12-26 22:39:52 +0800

    Make Hand rotate towards mouse pointer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -44,10 +44,17 @@ namespace CraftsmanHero {
             }
 
             // rotate hand
-            Vector2 lookDelta = GameManager.Instance.input.Player.Look.ReadValue<Vector2>();
-            if (lookDelta != Vector2.zero) {
-                //Debug.Log(lookDelta);
-            }
+            Vector2 mouseScreenPosition = GameManager.Instance.input.Player.Look.ReadValue<Vector2>();
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(
+                mouseScreenPosition.x, mouseScreenPosition.y, Camera.main.nearClipPlane
+                ));
+            float distance = Vector3.Distance(transform.position, mouseWorldPosition);
+
+            Vector3 direction = (mouseWorldPosition - transform.position).normalized;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            _hand.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
 
         private void FixedUpdate() {
